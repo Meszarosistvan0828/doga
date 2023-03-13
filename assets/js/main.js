d3328bb8f93b3b9885b1d7cff766d2bf6564d01e
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2023-03-13 14:46:33 +0100

    doga

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+function tolt()
+{
+    var z = document.getElementById("valasztott").value;
+    document.getElementById("kep").innerHTML== ""
+    if (z==1)
+    {
+        document.getElementById("kep").innerHTML="<img src='assets/images/polo-zold.png'>"
+    }
+    if (z==2)
+    {
+        document.getElementById("kep").innerHTML="<img src='assets/images/polo-kek.png'>"
+    }
+    if (z==3)
+    {
+        document.getElementById("kep").innerHTML="<img src='assets/images/polo-narancs.png'>"
+    }
+    if (z==4)
+    {
+        document.getElementById("kep").innerHTML="<img src='assets/images/polo-piros.png'>"
+    }
+    if (z==5)
+    {
+        document.getElementById("kep").innerHTML="<img src='assets/images/szurke.png'>"
+    }
+  
+
+}
+
+function kiir()
+{
+    
+}
+
+
